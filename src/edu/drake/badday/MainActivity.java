4fcdbcd0f6e2de26b6147c317b387cac5de31c87
Paxten <paxtenjohnson@Ps-Awesome-Computer.local>
2014-10-07 08:44:24 -0500

    tried to get decible value to show using TextView, problems accessing mic after start has been pushed

--- src/edu/drake/badday/MainActivity.java
+++ src/edu/drake/badday/MainActivity.java
@@ -1,23 +1,28 @@
 package edu.drake.badday;
 
-import java.io.File;
 import java.io.IOException;
+import java.util.Calendar;
 
+import android.app.Activity;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
-import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private TextView time, amp;
    private MediaRecorder myAudioRecorder;
    private String outputFile = null;
    private Button start,stop,play;
+   double amplitude;
+   int seconds1, seconds2;
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
@@ -38,8 +43,25 @@ public class MainActivity extends Activity {
       myAudioRecorder.setOutputFile(outputFile);
 
    }
+//      public double getAmplitude() {
+//      		//myAudioRecorder.prepare();
+//            //myAudioRecorder.start();
+//            if (myAudioRecorder != null){
+//                  return  (myAudioRecorder.getMaxAmplitude());}
+//            else{
+//                   return 0;}
+//
+//          }
+   
+
+  
 
    public void start(View view){
+	  ((Chronometer) findViewById(R.id.chronometer1)).start();
+	  Calendar startTime = Calendar.getInstance();
+	  startTime.getTime();
+	  seconds1 = startTime.get(Calendar.SECOND);
+
       try {
          myAudioRecorder.prepare();
          myAudioRecorder.start();
@@ -50,13 +72,34 @@ public class MainActivity extends Activity {
          // TODO Auto-generated catch block
          e.printStackTrace();
       }
+//	  if (myAudioRecorder != null){
+//		  amplitude = myAudioRecorder.getMaxAmplitude();}
+//	  
+//	  else { 
+//		  amplitude = 0;
+//	  }
       start.setEnabled(false);
       stop.setEnabled(true);
       Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+      
 
    }
 
+   public double getAmplitude() { 
+   if (myAudioRecorder != null){
+		  return myAudioRecorder.getMaxAmplitude();}
+	  
+	  else { 
+		  return 0;
+	  }
+   }
+	  
    public void stop(View view){
+	   ((Chronometer) findViewById(R.id.chronometer1)).stop();
+	   Calendar stopTime = Calendar.getInstance();
+	   stopTime.getTime();
+	   seconds2 = stopTime.get(Calendar.SECOND);
+
       myAudioRecorder.stop();
       myAudioRecorder.release();
       myAudioRecorder  = null;
@@ -64,7 +107,15 @@ public class MainActivity extends Activity {
       play.setEnabled(true);
       Toast.makeText(getApplicationContext(), "Audio recorded successfully",
       Toast.LENGTH_LONG).show();
+      
+      int totalTime = seconds2 - seconds1;
+      time = (TextView) findViewById(R.id.timeID);
+      time.setText(String.valueOf(totalTime));
+      double amplitude = getAmplitude();
+      amp = (TextView) findViewById(R.id.dBID);
+      amp.setText(String.valueOf(amplitude));
    }
+   
    @Override
    public boolean onCreateOptionsMenu(Menu menu) {
       // Inflate the menu; this adds items to the action bar if it is present.
@@ -81,5 +132,6 @@ public class MainActivity extends Activity {
    Toast.makeText(getApplicationContext(), "Playing audio", Toast.LENGTH_LONG).show();
 
    }
+
 
 }
