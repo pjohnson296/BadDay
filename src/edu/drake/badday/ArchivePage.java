9466822d89d79ddd67b36e16c44c566f061fc4c3
Paxten <paxtenjohnson@Ps-Awesome-Computer.local>
2014-10-15 18:57:53 -0500

    jjj

--- src/edu/drake/badday/ArchivePage.java
+++ src/edu/drake/badday/ArchivePage.java
@@ -10,7 +10,7 @@ public class ArchivePage extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_archive_page);
+		setContentView(R.layout.activity_page);
 	}
 
 	@Override
