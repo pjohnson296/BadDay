9e5f35d46028d4929242292b79a4c2066ddaf25d
Paxten <paxtenjohnson@PsAwesoComputer.ddns.drake.edu>
2014-10-26 21:43:47 -0500

    working on archive....still

--- src/edu/drake/badday/Archive.java
+++ src/edu/drake/badday/Archive.java
@@ -1,19 +1,43 @@
 package edu.drake.badday;
 
+import java.io.IOException;
+
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Archive extends Activity {
+	private String path;
+	int totalTime;
+	private MediaPlayer m;
+	boolean playback = false;
+	Button play;
+	//boolean clickable = false;
 
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_archive);
 		ActionBar actionBar = getActionBar();
 		actionBar.hide();
+		play = (Button)findViewById(R.id.play);
+		Intent intent = getIntent();                                                       //These two lines give a good idea
+		path = intent.getExtras().getString("path");                                       // of how to receive a passed string
+		totalTime = intent.getExtras().getInt("time");
+		TextView tv1 = (TextView)(findViewById(R.id.name));
+		//TextView time = (TextView)(findViewById(R.id.time));
+		//time.setText(totalTime);
+		tv1.setText("My Recording");
 	}
 
 	@Override
@@ -21,6 +45,38 @@ public class Archive extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.archive, menu);
 		return true;
+	}
+	
+	public void play(View view) throws IllegalArgumentException,   
+	SecurityException, IllegalStateException, IOException{
+		if (playback == false){
+		m = new MediaPlayer();
+		m.setDataSource(path);
+		m.prepare();
+		m.start();
+		Toast.makeText(getApplicationContext(), "Playing audio", Toast.LENGTH_LONG).show();
+		playback = true;
+		play.setBackgroundResource(R.drawable.stopp);
+		}
+		
+		else if (playback == true){
+			try {
+				if (m != null) {
+					m.stop();
+					m.release();
+					m = null;
+					// reset.setEnabled(true);              //enables the reset of the recording, we don't have that yet
+					Toast.makeText(getApplicationContext(), "Stop playing the recording...",	   
+							Toast.LENGTH_SHORT).show();
+					playback = false;
+					play.setBackground(null);
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+		}
+
 	}
 
 	@Override
