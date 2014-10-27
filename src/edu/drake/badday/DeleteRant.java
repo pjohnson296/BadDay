c031042ad91442ea0b25a261d100ac6de8c24f71
Kanmani92 <kanmani.muthusamy@drake.edu>
2014-10-26 18:19:14 -0700

    added confirm button

--- src/edu/drake/badday/DeleteRant.java
+++ src/edu/drake/badday/DeleteRant.java
@@ -2,12 +2,19 @@ package edu.drake.badday;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class DeleteRant extends Activity {
 
+	
+	public void flames(View view){
+		Intent intent = new Intent(this, Flame.class);
+		startActivity(intent);
+	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
